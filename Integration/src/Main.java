dae8883b490daac862ced38894f051543b9d50d3
bvelez <Bienvenido.Velez@upr.edu>
2017-06-10 10:09:11 -0400

    Added examples of lambda expressions

--- Integration/src/Main.java
+++ Integration/src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
 		double result1 = i.integrate(sqrt, 0.0, 2.0, 1000);
 
-		double result3 = i.integrate(
+		double result2 = i.integrate(
 				new Integrable() { 
 					public double eval(double x) {
 						return Math.sqrt(x);
@@ -19,8 +19,16 @@ public class Main {
 
 				0.0, 2.0, 1000);
 
-		double result2 = i.integrate(square, 0.0, 2.0, 1000);
+		double result3 = i.integrate(x -> Math.sqrt(x), 0.0, 2.0, 1000);
 
+		double result4 = i.integrate(x -> (x * x - x + 1), 0.0, 2.0, 1000);
+
+		double result5 = i.integrate(square, 0.0, 2.0, 1000);
+
+		double result6 = i.integrate(x -> (0.0 / 0.0), 0.0, 2.0, 1000);
+
+		System.out.println(result6);
+		
 		System.out.println("Integral of square root from 0 to 2 = " + result1);
 		System.out.println("Integral of square from 0 to 2 = " + result2);
 
